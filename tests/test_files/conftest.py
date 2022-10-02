edd1d32a2a9029cc99d235d5e0d0f7f65814a565
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2022-10-02 19:48:20 +0200

    feat: expand base file class

---
+++ tests/test_files/conftest.py
@@ -0,0 +1,65 @@
+import pytest
+
+from tm1filetools.tools.filetool import TM1FileTool
+
+cub_files = ["cat", "dog"]
+rux_files = ["dog", "giraffe"]
+dim_files = ["koala", "possum"]
+blb_files = ["emu", "unicorn"]
+sub_files = ["platypus", "donkey"]
+dim_attributes = ["koala", "kangaroo"]
+cub_attributes = ["koala", "humphrey"]
+
+
+@pytest.fixture(scope="function")
+def test_folder(tmp_path_factory):
+    """
+    Create a bunch of temp files that can be used to test the filesys functions with session scope
+    """
+
+    # create a temp path
+    d = tmp_path_factory.mktemp("data")
+
+    # create the temp files
+
+    for c in cub_files:
+
+        f = d / f"{c}.cub"
+        f.touch()
+
+    for r in rux_files:
+
+        f = d / f"{r}.rux"
+        f.touch()
+
+    for df in dim_files:
+
+        f = d / f"{df}.dim"
+        f.touch()
+
+    for b in blb_files:
+
+        f = d / f"{b}.blb"
+        f.touch()
+
+    for s in sub_files:
+
+        f = d / f"{s}.sub"
+        f.touch()
+
+    for da in dim_attributes:
+
+        da = TM1FileTool.attr_prefix + da
+
+        f = d / f"{da}.dim"
+        f.touch()
+
+    for ca in cub_attributes:
+
+        ca = TM1FileTool.attr_prefix + ca
+
+        f = d / f"{ca}.cub"
+        f.touch()
+
+    # return the path
+    return d
--- tests/test_files/test_base.py
+++ tests/test_files/test_base.py
@@ -1,8 +1,82 @@
+from pathlib import Path
+
 from tm1filetools.files.base import TM1File
 
 
-def test_init(tm1_file_tool_test):
+def test_exists(test_folder):
 
-    f = TM1File("cat.cub")
+    f = TM1File(Path.joinpath(test_folder, "cat.cub"))
 
-    assert f
+    assert f.exists()
+
+    f = TM1File(Path.joinpath(test_folder, "unicorn.cub"))
+
+    assert not f.exists()
+
+
+def test_delete(test_folder):
+
+    f = TM1File(Path.joinpath(test_folder, "cat.cub"))
+
+    assert f.exists()
+
+    f.delete()
+
+    assert not f.exists()
+
+
+def test_name(test_folder):
+
+    f = TM1File(Path.joinpath(test_folder, "cat.cub"))
+
+    assert f.name == "cat.cub"
+
+
+def test_stem(test_folder):
+
+    f = TM1File(Path.joinpath(test_folder, "cat.cub"))
+
+    assert f.stem == "cat"
+
+
+def test_prefix(test_folder):
+
+    f = TM1File(Path.joinpath(test_folder, "cat.cub"))
+
+    assert f.prefix is None
+
+    f = TM1File(Path.joinpath(test_folder, "}ElementAttributes_cat.cub"))
+
+    assert f.prefix == f.prefixes["attr_prefix"]
+
+
+def test_suffix(test_folder):
+
+    f = TM1File(Path.joinpath(test_folder, "cat.cub"))
+
+    assert f.suffix == "cub"
+
+
+def test_is_control(test_folder):
+
+    f = TM1File(Path.joinpath(test_folder, "cat.cub"))
+
+    assert not f.is_control_object()
+
+    f = TM1File(Path.joinpath(test_folder, "}ElementAttributes_koala.cub"))
+
+    assert f.is_control_object()
+
+
+def test_is_tm1_file(test_folder):
+
+    f = TM1File(Path.joinpath(test_folder, "cat.cub"))
+
+    print(f.suffix)
+
+    # print("cub" in f.suffixes.values())
+    assert f.is_tm1_file()
+
+    f = TM1File(Path.joinpath(test_folder, "unicorn.txt"))
+
+    assert not f.is_tm1_file()
