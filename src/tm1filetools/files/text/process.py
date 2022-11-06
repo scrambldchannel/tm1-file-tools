2c039b9226e113c73ddd0f91805b64955632cd7a
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2022-11-06 20:40:24 +0100

    feat: add very naive parsing func

--- src/tm1filetools/files/text/process.py
+++ src/tm1filetools/files/text/process.py
@@ -5,15 +5,29 @@ from .text import TM1TextFile
 
 class TM1ProcessFile(TM1TextFile):
     """
-    A class representation of a tm1 TI process file
+    A class representation of a tm1 TI process file. A TM1 .pro file
+
 
     """
 
     suffix = "pro"
 
+    # line codes
+
     def __init__(self, path: Path):
 
         super().__init__(path)
 
-    # There's scope to do some naive "parsing" of the text
-    # and potentially even create a tool for hot promotion using TM1py
+    # Attempt some simple parsing
+
+    # https://gist.github.com/scrambldchannel/9955cb731f80616c706f2d5a81b82c2a
+
+    def _find_line_by_code(self, linecode):
+
+        lines = []
+        # just need to remember how to read a file line by line here
+        for line in self.readlines():
+            if line[0:3] == linecode:
+                lines.append(line)
+
+        return lines
--- tests/test_files/test_text/test_process.py
+++ tests/test_files/test_text/test_process.py
@@ -9,3 +9,18 @@ def test_init(test_folder):
 
     assert p
     assert p.suffix == "pro"
+
+
+def test_find_line(test_folder):
+
+    p = TM1ProcessFile(Path.joinpath(test_folder, "copy data from my cube.pro"))
+
+    # build up this file and read some basic line codes
+
+    p.write(
+        """601,100
+        602,my zany process
+        """
+    )
+
+    assert p._find_line_by_code(linecode="601")[0] == "601,100"
