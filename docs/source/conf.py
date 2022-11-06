121c93d35ad52891ffe2fda518deedcba7a8b49d
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2022-11-06 12:12:35 +0100

    docs: add sphinx quickstart

---
+++ docs/source/conf.py
@@ -0,0 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "TM1 File Tools"
+copyright = "2022, Alexander Sutcliffe"
+author = "Alexander Sutcliffe"
+release = "0.3.2"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
---
+++ docs/source/index.rst
@@ -0,0 +1,28 @@
+.. TM1 File Tools documentation master file, created by
+   sphinx-quickstart on Sun Nov  6 12:04:06 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to TM1 File Tools' documentation!
+==========================================
+
+A Python package that simplifies working with files associated with a
+TM1 server. It’s primarily useful for linting or cleaning up a server
+directory without a dependency on a running TM1 Server.
+
+.. note::
+
+   This project is under active development.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
