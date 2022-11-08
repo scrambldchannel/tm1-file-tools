927dfeba145a9d11b6099f853abecb5a6bbb9254
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2022-11-08 19:08:13 +0100

    feat: get mdx for subset

--- tests/test_files/test_text/test_subset.py
+++ tests/test_files/test_text/test_subset.py
@@ -1,3 +1,4 @@
+# import json
 from pathlib import Path
 
 from tm1filetools.files import TM1SubsetFile
@@ -74,3 +75,45 @@ def test_move_to_public(test_folder):
     assert f._get_owner_name() is None
     assert f._path == Path.joinpath(test_folder, f"cat{TM1SubsetFile.folder_suffix}", "platypus.sub")
     assert f.public
+
+
+def test_single_static_subset(json_dumps_folder):
+
+    subset = "test.tm1filetools.single_element_static_subset"
+
+    sub = TM1SubsetFile(Path.joinpath(json_dumps_folder, "subsets", f"{subset}.sub"))
+
+    assert sub
+
+    with open(Path.joinpath(json_dumps_folder, "subsets", f"{subset}.json"), "r") as f:
+        expected_json_str = f.read()
+
+    assert expected_json_str
+
+
+def test_multi_static_subset(json_dumps_folder):
+
+    subset = "test.tm1filetools.multi_element_static_subset"
+
+    sub = TM1SubsetFile(Path.joinpath(json_dumps_folder, "subsets", f"{subset}.sub"))
+
+    assert sub
+
+    with open(Path.joinpath(json_dumps_folder, "subsets", f"{subset}.json"), "r") as f:
+        expected_json_str = f.read()
+
+    assert expected_json_str
+
+
+def test_mdx_subset(json_dumps_folder):
+
+    subset = "test.tm1filetools.mdx_subset"
+
+    sub = TM1SubsetFile(Path.joinpath(json_dumps_folder, "subsets", f"{subset}.sub"))
+
+    assert sub._get_mdx() == "{TM1SUBSETALL( [}Processes] )}"
+
+    with open(Path.joinpath(json_dumps_folder, "subsets", f"{subset}.json"), "r") as f:
+        expected_json_str = f.read()
+
+    assert expected_json_str
