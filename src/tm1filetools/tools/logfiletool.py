a90a9567950f4150307035a1bc6770fd3f2fd51c
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2022-11-11 18:58:24 +0100

    feat: add get proc error

--- src/tm1filetools/tools/logfiletool.py
+++ src/tm1filetools/tools/logfiletool.py
@@ -30,6 +30,10 @@ class TM1LogFileTool(TM1BaseFileTool):
         # logs - can be split into categories
         self._log_files: Optional[list] = None
 
+        self._tm1_log = None
+        self._process_error_logs = None
+        self._cube_change_logs = None
+
     def find_all(self):
         """
         Do a full scan of the dir(s) and populate all lists of files
@@ -49,19 +53,35 @@ class TM1LogFileTool(TM1BaseFileTool):
 
         return self._log_files
 
+    def get_process_error_logs(self):
+        """Return a list of all process error logs"""
+
+        self._find_logs()
+
+        return self._process_error_logs
+
     def _find_logs(self):
 
         # logs may be in a different path so search with the glob func
         # We should also be careful of the tm1s.log file as we may fail to get a lock on it
 
-        logs = []
+        tm1_log = []
+        process_error_logs = []
+        cube_change_logs = []
         for log in self._case_insensitive_glob(self._path, f"*.{TM1LogFile.suffix}"):
             # if we think this is the tm1s.log file, use the derived class that avoids trying to open it
             if log.stem.lower() == "tm1s":
-                logs.append(TM1ChangeLogFile(log))
+                tm1_log.append(TM1ChangeLogFile(log))
             elif log.stem.lower().startswith(TM1ProcessErorrLogFile.prefix.lower()):
-                logs.append(TM1ProcessErorrLogFile)
+                process_error_logs.append(TM1ProcessErorrLogFile)
             else:
-                logs.append(TM1LogFile(log))
+                cube_change_logs.append(TM1LogFile(log))
+
+        self._tm1_log = tm1_log
+        self._process_error_logs = process_error_logs
+        self._cube_change_logs = cube_change_logs
+
+        # retain this for backwards compatibilty but maybe remove
+        logs = tm1_log + process_error_logs + cube_change_logs
 
         self._log_files = logs
